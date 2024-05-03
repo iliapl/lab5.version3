d1e8111af1b86639448ccd11997c9cf9875f9a0e
ahahac_be3_xboctuka <godunovb11@gmail.com>
2024-05-04 00:28:35 +0300

    по ананасим?

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,6 +3,7 @@ import util.Creatore;
 
 public class Client {
     public static void main(String[] args){
+        System.out.println("гоу-гоу");
         Creatore creatore = new Creatore();
         creatore.create();
         Console console = new Console(creatore.comands, creatore.scanner);
