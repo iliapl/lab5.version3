5b310723e57cc8eb4b23cb47a87df2273c21d7ac
ahahac_be3_xboctuka <godunovb11@gmail.com>
2024-09-24 22:52:59 +0300

    We must pull up our pants

--- src/main/java/forCommands/Commands/Help.java
+++ src/main/java/forCommands/Commands/Help.java
@@ -8,11 +8,11 @@ public class Help implements Command {
         System.out.println("info: Выводит информацию о коллекции");
         System.out.println("show: Выводит все элементы коллекции");
         System.out.println("add: Добавляет элемент в коллекцию");
-        System.out.println("updateid (+ пробел + число): Обновляет значение элемента коллекции, id которого равен заданному");
-        System.out.println("removebyid (+ пробел + число): Удаляет элемент из коллекции по его id");
+        System.out.println("updateid: Обновляет значение элемента коллекции, id которого равен заданному");
+        System.out.println("removebyid: Удаляет элемент из коллекции по его id");
         System.out.println("clear: Очищает коллекцию");
         System.out.println("save: Сохраняет коллекцию в файл");
-        System.out.println("executeScriptFileName + имя скрипта: Считывает и исполняет скрипт из указанного файла. В скрипте содержатся команды в таком же виде, в котором их вводит пользователь");
+        System.out.println("executescript: Считывает и исполняет скрипт из указанного файла. В скрипте содержатся команды в таком же виде, в котором их вводит пользователь");
         System.out.println("addifmin: Добавляет новый элемент в коллекцию, если его значение меньше, чем у наименьшего элемента этой коллекции");
         System.out.println("removegreater: Удаляет из коллекции все элементы, превышающие заданный");
         System.out.println("exit: Завершает выполнение программы без сохранения в файл");
