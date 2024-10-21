53373e0b3843a1ebc0bf46baf4180dcd4762e00a
ahahac_be3_xboctuka <godunovb11@gmail.com>
2024-10-21 15:34:29 +0300

    последний штрих

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@ import Utilities.Creator;
 public class App {
     public static void main(String[] args){
         System.out.println("гоу-гоу");
+        System.out.println("жопа");
         Creator creator = new Creator();
         creator.create();
         Console console = new Console(creator.scanner, creator.executeCommands);
